2762dfbc6582bca9964b866479b7498f4c8cf759
Rajkumarthakur88 <thakurraj8878@gmail.com>
2022-11-16 20:40:46 +0530

    error has been handled

--- main.js
+++ main.js
@@ -15,58 +15,68 @@ async function getData(e) {
     let weatherData = await fetch(`https://api.weatherapi.com/v1/current.json?key=22af615fe10e4b5593990007221606&q=${input.value}&aqi=yes`);
     let weatherJson = await weatherData.json();
 
-    // GETDATA AND STORE IN VARIABLES
-    let temp_C = weatherJson.current.temp_c;
-    let temp_F = weatherJson.current.temp_f;
-    let wind_S = weatherJson.current.wind_kph;
-    let Location = weatherJson.location.name;
-    let region = weatherJson.location.region;
-    let weather_Icon = weatherJson.current.condition.icon;
-    let weather_Condition = weatherJson.current.condition.text;
+    //    console.log();
 
-    console.log(weatherJson);
-
-    // SELECT HTML ELEMENTS TO MANIPULATE THEM 
-    let temp_c = document.querySelector(".temp_c");
-    temp_c.innerText = temp_C + "°C";
-
-    let temp_f = document.querySelector(".temp_f");
-    temp_f.innerText = temp_F + "°F";
-
-    let WindSpeed = document.querySelector(".wind_speed");
-    WindSpeed.innerText = "Wind:-" + " " + wind_S + " " + "km/hr";
-
-    let location = document.querySelector(".location");
-    location.innerText = Location + ", " + region;
-
-    let Icon = document.querySelector(".icon");
-    Icon.setAttribute("src", weather_Icon);
-
-    let Condition = document.querySelector(".weather_condition");
-    Condition.innerText = weather_Condition;
-
-
-    // console.log(temp_C);
-    // console.log(temp_F);
-    // console.log(wind_S);
-    // console.log(date_Time);
-    // console.log(weather_Icon);
-    // console.log(weather_Condition);
-    // console.log(weatherJson);
-
-    form.reset();
-
-
-    let time = weatherJson.current.is_day;
-    let container = document.querySelector(".container");
-    console.log(time);
-    if (time == 0) {
-        container.style.backgroundImage = "url(./Night-bimage.png)";
+    if (weatherJson.error) {
+        window.alert("Enter Correct City")
     }
     else {
-        container.style.backgroundImage = "url(./daybg.jpg)";
+        // GETDATA AND STORE IN VARIABLES
+        let temp_C = weatherJson.current.temp_c;
+        let temp_F = weatherJson.current.temp_f;
+        let wind_S = weatherJson.current.wind_kph;
+        let Location = weatherJson.location.name;
+        let region = weatherJson.location.region;
+        let weather_Icon = weatherJson.current.condition.icon;
+        let weather_Condition = weatherJson.current.condition.text;
+
+        // console.log(weatherJson);
+
+        // SELECT HTML ELEMENTS TO MANIPULATE THEM 
+        let temp_c = document.querySelector(".temp_c");
+        temp_c.innerText = temp_C + "°C";
+
+        let temp_f = document.querySelector(".temp_f");
+        temp_f.innerText = temp_F + "°F";
+
+        let WindSpeed = document.querySelector(".wind_speed");
+        WindSpeed.innerText = "Wind:-" + " " + wind_S + " " + "km/hr";
+
+        let location = document.querySelector(".location");
+        location.innerText = Location + ", " + region;
+
+        let Icon = document.querySelector(".icon");
+        Icon.setAttribute("src", weather_Icon);
+
+        let Condition = document.querySelector(".weather_condition");
+        Condition.innerText = weather_Condition;
+
+
+        let reportContainer = document.querySelector(".weather-report");
+        reportContainer.style.visibility = "visible";
+
+
+        // console.log(temp_C);
+        // console.log(temp_F);
+        // console.log(wind_S);
+        // console.log(date_Time);
+        // console.log(weather_Icon);
+        // console.log(weather_Condition);
+        // console.log(weatherJson);
+
+        form.reset();
+
+
+        let time = weatherJson.current.is_day;
+        let container = document.querySelector(".container");
+        // console.log(time);
+        if (time == 0) {
+            container.style.backgroundImage = "url(./Night-bimage.png)";
+        }
+        else {
+            container.style.backgroundImage = "url(./daybg.jpg)";
+        }
+
     }
-
-
 }
 
